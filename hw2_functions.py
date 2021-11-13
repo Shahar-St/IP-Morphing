ad1be193117be4afe1ab642dd1da8d01a4ba8990
Shahar Stahi <shaharstahi@gmail.com>
2021-11-13 11:38:16 +0200

    Added findAffineTransform

--- hw2_functions.py
+++ hw2_functions.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 def print_IDs():
     print("305237257+312162027\n")
 
-# def writeMorphingVideo(image_list, video_name):
-#     out = cv2.VideoWriter(video_name+".mp4", cv2.VideoWriter_fourcc(*'MP4V'), 20.0, image_list[0].shape, 0)
-#     for im in image_list:
-#         out.write(im)
-#     out.release()
-#
-#
+def writeMorphingVideo(image_list, video_name):
+    out = cv2.VideoWriter(video_name+".mp4", cv2.VideoWriter_fourcc(*'MP4V'), 20.0, image_list[0].shape, 0)
+    for im in image_list:
+        out.write(im)
+    out.release()
+
+
 # def createMorphSequence (im1, im1_pts, im2, im2_pts, t_list, transformType):
 #     if transformType:
 #         # TODO: projective transforms
@@ -58,15 +58,27 @@ def print_IDs():
 #     return T
 #
 #
-# def findAffineTransform(pointsSet1, pointsSet2):
-#     N = pointsSet1.shape[0]
-#
-#     # iterate iver points to create x , x'
-#     for i in range(0, N):
-#
-#
-#     # calculate T - be careful of order when reshaping it
-#     return T
+def findAffineTransform(pointsSet1, pointsSet2):
+    N = pointsSet1.shape[0]
+
+    # iterate over points to create x , x'
+    first_row = [0, 0, 0, 0, 1, 0]
+    sec_row = [0, 0, 0, 0, 0, 1]
+    x = np.array([
+        first_row, sec_row,
+        first_row, sec_row,
+        first_row, sec_row
+    ])
+    points_location = 0
+    for i in range(0, 3):
+        x[points_location][0] = x[points_location + 1][2] = pointsSet1[i][0]
+        x[points_location][1] = x[points_location + 1][3] = pointsSet1[i][1]
+        points_location += 2
+
+    x_t = pointsSet2.reshape(N * 2)[:6]
+    T = np.matmul(np.linalg.pinv(x), x_t)
+
+    return T
 
 
 def getImagePts(im1, im2, varName1, varName2, nPoints):
@@ -79,5 +91,5 @@ def getImagePts(im1, im2, varName1, varName2, nPoints):
     print(imagePts1)
     print(imagePts2)
 
-    np.save(varName1 + ".npy", imagePts1)
-    np.save(varName2 + ".npy", imagePts2)
+    np.save(varName1, imagePts1)
+    np.save(varName2, imagePts2)
--- hw2_script.py
+++ hw2_script.py
@@ -20,10 +20,32 @@ def main():
     if not os.path.exists(points_dir):
         os.makedirs(points_dir)
 
-    path_to_points1 = os.path.join(points_dir, img_1_name + '_Points')
-    path_to_points2 = os.path.join(points_dir, img_2_name + '_Points')
-    getImagePts(img1, img2, path_to_points1, path_to_points2, nPoints=1)
+    path_to_points1 = os.path.join(points_dir, img_1_name + '_Points.npy')
+    path_to_points2 = os.path.join(points_dir, img_2_name + '_Points.npy')
+    # getImagePts(img1, img2, path_to_points1, path_to_points2, nPoints=4)
+
+    point_set1 = np.load(path_to_points1)
+    point_set2 = np.load(path_to_points2)
+    findAffineTransform(point_set1, point_set2)
+    z = 3
 
 
 if __name__ == '__main__':
+    a = np.array(
+        [[1, 2], [3, 4], [5, 6]]
+    )
+    b = np.array(
+        [[7, 8], [9, 10], [11, 12]]
+    )
+    x_1 = [
+        [1, 2, 0, 0, 1, 0],
+        [0, 0, 1, 2, 0, 1],
+        [3, 4, 0, 0, 1, 0],
+        [0, 0, 3, 4, 0, 1],
+        [5, 6, 0, 0, 1, 0],
+        [0, 0, 5, 6, 0, 1],
+    ]
+
+    x_t_1 = [7, 8, 9, 10, 11, 12]
+
     main()
